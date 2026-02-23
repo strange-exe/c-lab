db16518b13dfe5bf1d9256dd4facb50ca1a27ba4
Abhinesh <work.abhinesh@gmail.com>
2026-02-23 11:45:23 +0530

    Optimised

--- Array/add.c
+++ Array/add.c
@@ -1,34 +1,27 @@
 #include <stdio.h>
-
 int main() {
     int n, m;
     printf("Enter length of array 1: ");
     scanf("%d", &n);
     int a[n];
-    for (int i=0; i<n; i++){
-        printf("Enter ele %d: ",i+1);
+    for (int i = 0; i < n; i++)
         scanf("%d", &a[i]);
-    }
-    printf("\nEnter length of array 2: ");
+    printf("Enter length of array 2: ");
     scanf("%d", &m);
     int b[m];
-    for (int i=0; i<m;i++){
-        printf("Enter ele %d: ",i+1);
+    for (int i = 0; i < m; i++)
         scanf("%d", &b[i]);
-    }
-    int max=(n>m)?n:m,min=(n<m)?n:m;
+    int max = (n > m) ? n : m;
     int c[max];
-    for (int i=0; i<min; i++)
-        c[i]=a[i]+b[i];
-    if (n>m){
-        for (int i=min; i<n; i++)
-            c[i]=a[i];
+    for (int i = 0; i < max; i++) {
+        if (i < n && i < m)
+            c[i] = a[i] + b[i];
+        else if (i < n)
+            c[i] = a[i];
+        else
+            c[i] = b[i];
     }
-    else{
-        for (int i=min; i<m; i++)
-            c[i]=b[i];
-    }
-    printf("\nResult array:\n");
-    for (int i=0; i<max; i++)
+    printf("Result array:\n");
+    for (int i = 0; i < max; i++)
         printf("%d ", c[i]);
 }
