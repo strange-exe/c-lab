111a6b32ac8a682118b78cbb60f47a130203d2e8
Abhinesh <work.abhinesh@gmail.com>
2026-03-25 13:43:29 +0530

    Add files via upload

---
+++ Array/Strings/swapcase.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+char a[100];
+printf("Enter string : ");
+gets(a);
+int i=0;
+while (a[i]){
+if (a[i]>=65 && a[i]<=90) a[i]+=32;
+else if (a[i]>=97 && a[i]<=122) a[i]-=32;
+else continue;
+}
+printf("New string %s\n",a);
+}
